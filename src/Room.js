a9b92e63bc7cac9f9fa99ae28c6d0da7592a7992
bawitdaba <bawitdaba@gmail.com>
2019-05-14 20:10:38 -0400

    Update Room.js

--- src/Room.js
+++ src/Room.js
@@ -61,6 +61,7 @@ const Room = ({ color, number, right, locked }) => {
 								alignItems: 'center',
 								width: 20,
 								height: 20,
+								transform: rotate(${right ? '-90deg' : '90deg'}),
 								color:
 									locked[`${color}-${terminal}`] != null
 										? color
